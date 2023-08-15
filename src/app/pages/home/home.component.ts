6ac224bf903f6541de2cf12ad65f0fb0077ce893
Carlos Gómez <drcgomez@hotmail.com>
2023-08-15 11:08:30 -0500

    Modificando el home
    Refactorización del componente products para pasarlo al componente home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Product } from '../../models/product.model';
+
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  products: Product[] = [];
+  limit = 10;
+  offset = 0;
+
+  constructor(
+    private productsService: ProductsService
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    this.productsService.getAll(10, 0).subscribe((data) => {
+      this.products = data;
+      this.offset += this.limit;
+    });
+  }
+
+  onLoadMore() {
+    this.productsService.getAll(this.limit, this.offset)
+      .subscribe(data => {
+        this.products = this.products.concat(data.filter(product => product.images.length > 0));
+        this.offset += this.limit;
+      });
+  }
 
 }
