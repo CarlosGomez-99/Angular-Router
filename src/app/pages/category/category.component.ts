df2b8498556b990818e19f44cfa113df76768cad
Carlos Gómez <drcgomez@hotmail.com>
2023-08-15 19:18:24 -0500

    Mostrando productos por categoria

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ProductsService } from './../../services/products.service';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-category',
@@ -7,4 +11,35 @@ import { Component } from '@angular/core';
 })
 export class CategoryComponent {
 
+  categoryId: string | null = null;
+  limit = 10;
+  offset = 0;
+  products: Product[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) {
+    this.onLoadMore();
+  }
+
+  onLoadMore() {
+    this.route.paramMap.subscribe(params => {
+      this.categoryId = params.get('id');
+      if (this.categoryId) {
+        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+          .subscribe(data => {
+            console.log(data);
+
+            let productsCount = this.products.length;
+            if (productsCount === 0) {
+              this.products = data;
+            } else {
+              this.products = this.products.concat(data);
+            }
+            this.offset += this.limit;
+          });
+      }
+    });
+  }
 }
