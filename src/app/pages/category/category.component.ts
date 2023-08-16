899e84e073401575920f4a7b869ea3b8721d04b3
Carlos Gómez <drcgomez@hotmail.com>
2023-08-15 19:30:38 -0500

    Evitando callback hell

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 import { ProductsService } from './../../services/products.service';
 import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-category',
-  templateUrl: './category.component.html',
+  template: '<app-products [products]="products" (loadMore)="onLoadMore()"></app-products>',
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent {
@@ -24,22 +25,24 @@ export class CategoryComponent {
   }
 
   onLoadMore() {
-    this.route.paramMap.subscribe(params => {
-      this.categoryId = params.get('id');
-      if (this.categoryId) {
-        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-          .subscribe(data => {
-            console.log(data);
-
-            let productsCount = this.products.length;
-            if (productsCount === 0) {
-              this.products = data;
-            } else {
-              this.products = this.products.concat(data);
-            }
-            this.offset += this.limit;
-          });
-      }
-    });
+    this.route.paramMap
+      .pipe(
+        switchMap(params => {
+          this.categoryId = params.get('id');
+          if (this.categoryId) {
+            return this.productsService.getByCategory(this.categoryId, this.limit, this.offset);
+          }
+          return [];
+        })
+      )
+      .subscribe((data) => {
+        let productsCount = this.products.length;
+        if (productsCount === 0) {
+          this.products = data;
+        } else {
+          this.products = this.products.concat(data);
+        }
+        this.offset += this.limit;
+      });
   }
 }
